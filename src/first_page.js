3eca056c925b8ab077030f7bf71a23652ff0ce43
rohit07052 <rohitsingh07052@gmail.com>
2023-12-07 20:24:12 +0530

    first commit

---
+++ src/first_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FirstPage.css';
+//import logo from './img/Fill 213.png'
+
+function FirstPage(){
+    return (
+        <body>
+        <div class="heart"></div>
+        <button class="button-33" >click me baby</button>
+        </body>
+      
+    )
+}
+
+export default FirstPage;
